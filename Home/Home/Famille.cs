85f07583d59b4853e4ec3ef988b4e5bec5a9881e
Julien DAVID <hidjudju@protonmail.com>
2024-10-05 18:06:28 +0200

    test

---
+++ Home/Home/Famille.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home
+{
+    internal class Famille
+    {
+        string nom;
+        string description;
+        List<Personne> personnes;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Description { get => description; set => description = value; }
+        internal List<Personne> Personnes { get => personnes; set => personnes = value; }
+    }
+}
---
+++ Home/Home/Personne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home
+{
+    internal class Personne
+    {
+        string prenom;
+        string nom;
+        string age;
+        string genre;
+        List<Hobby> hobbies;
+
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Age { get => age; set => age = value; }
+        public string Genre { get => genre; set => genre = value; }
+        internal List<Hobby> Hobbies { get => hobbies; set => hobbies = value; }
+    }
+}
